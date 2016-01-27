e2250ec492128f811311289737459303f3a31842
AnnMarieLy <annmariely@aol.com>
2016-01-27 06:46:01 +0000

    node.js lab

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
-var index = require('./routes/index');
+//var index = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
+
+var index = require('./routes/index');
+var project = require('./routes/project');
 
 var app = express();
 
@@ -36,6 +39,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
